6ede0c7addde1ddcb83ae974d9ff1fb4858114f1
NoFox <nfox598625@gmail.com>
2024-02-14 23:56:18 -0500

    finished index.js and svg.js

--- index.js
+++ index.js
@@ -1,27 +1,3 @@
-const inquirer = require("inquirer");
-const jest = require("jest");
-const shapes = require("./lib/shapes");
+const CTS = require("./lib/index");
 
-const questions = [
-  {
-    type: "input",
-    name: "title",
-    message: "Enter 3 Characters!",
-  },
-  {
-    type: "input",
-    name: "text",
-    message: "Enter in a color keyword (OR a hexadecimal number).",
-  },
-  {
-    type: "list",
-    name: "shapes",
-    message: "Please select from the list of shapes.",
-    choices: ["Circle", "Square", "Triangle"],
-  },
-  {
-    type: "input",
-    name: "shape",
-    message: "Enter in a color keyword (OR a hexadecimal number).",
-  },
-];
+new CTS().run();
